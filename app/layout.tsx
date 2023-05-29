164f5caed72f7621eb924136cfff6d6078718e0b
OhmKhay <chestersai12@gmail.com>
2023-05-30 01:53:33 +0630

    feat: add meta tag and desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* https://haohaa.sgp1.digitaloceanspaces.com/post_images/tts-shan-bg-png-ukb80-20230530 */}
       <Script src="https://cdn.jsdelivr.net/gh/haohaaorg/shanormyanmar@master/dist/som.min.js" />
       <link rel="icon" href="/favicon.ico" sizes="any" />
+      <meta name="description" content={"Shan Text To Speech"} />
+      <meta name="keywords" content="HaoHaa, Science, TTS, Shan" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0,user-scalable=0"
+      />
+
+      <meta property="og:title" content={"Shan Text To Speech"} />
+      <meta property="og:site_name" content="HaoHaa" />
+      <meta property="og:type" content={"website"} />
+      <meta property="og:url" content={"https://haohaa.com"} />
+      <meta
+        property="og:image"
+        content={
+          " https://haohaa.sgp1.digitaloceanspaces.com/post_images/tts-shan-bg-png-ukb80-20230530"
+        }
+      />
+      <meta property="og:image:alt" content={"Shan Text to Speech"} />
+      <meta property="og:description" content={"Shan Text To Speech"} />
       <body
         className={`${inter.className} font-mont bg-light dark:bg-dark w-full min-h-screen`}
         suppressHydrationWarning={true}
