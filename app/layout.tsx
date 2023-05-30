1185723efdfd121c9f091f31b2523dbef6a7647b
OhmKhay <chestersai12@gmail.com>
2023-05-30 09:49:28 +0630

    fix: meta desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,25 +20,34 @@ export default function RootLayout({
       {/* https://haohaa.sgp1.digitaloceanspaces.com/post_images/tts-shan-bg-png-ukb80-20230530 */}
       <Script src="https://cdn.jsdelivr.net/gh/haohaaorg/shanormyanmar@master/dist/som.min.js" />
       <link rel="icon" href="/favicon.ico" sizes="any" />
-      <meta name="description" content={"Shan Text To Speech"} />
+      <meta
+        name="description"
+        content={"A Shan Text To Speech Quick Playground"}
+      />
       <meta name="keywords" content="HaoHaa, Science, TTS, Shan" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0,user-scalable=0"
       />
 
-      <meta property="og:title" content={"Shan Text To Speech"} />
+      <meta property="og:title" content={"Shan Text To Speech - HaoHaa"} />
       <meta property="og:site_name" content="HaoHaa" />
       <meta property="og:type" content={"website"} />
-      <meta property="og:url" content={"https://haohaa.com"} />
+      <meta
+        property="og:url"
+        content={"https://shantts-playground.haohaa.com/"}
+      />
       <meta
         property="og:image"
         content={
           "https://haohaa.sgp1.digitaloceanspaces.com/post_images/shn-tts-bg-png-efz8e-20230530"
         }
       />
-      <meta property="og:image:alt" content={"Shan Text to Speech"} />
-      <meta property="og:description" content={"Shan Text To Speech"} />
+      <meta property="og:image:alt" content={"Shan Text to Speech - HaoHaa"} />
+      <meta
+        property="og:description"
+        content={"A Shan Text To Speech Quick Playground"}
+      />
 
       <meta
         name="viewport"
